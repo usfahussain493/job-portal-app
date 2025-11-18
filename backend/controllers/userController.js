8ae0035020020c8379c1eb70e47633aef8e49074
Usfahussain493 <bc200203561@vu.edu.pk>
2025-11-18 11:31:41 +0500

    Updated userController and error middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,4 +2,28 @@ import {catchAsyncErrors} from "../middlewares/catchAsyncError.js";
 import ErrorHandler from "../middlewares/error.js";
 import { User } from "../models/userSchema.js";
 
-export const register = catchAsyncErrors()
+export const register = catchAsyncErrors(async(req, res, next)=>{
+    try {
+        const {
+            name, 
+            email,
+            phone,
+            address,
+            password, 
+            role, 
+            firstNiche, 
+            secondNiche, 
+            thirdNiche, 
+            coverLetter,
+        } =req.body;
+
+        if(!name || !email || !phone || !address || !role){
+            return next(new ErrorHandler("All fields are required.", 400));
+        }
+        if(role === "Job Seeker" && (!firstNiche || !secondNiche || !thirdNiche)){
+            return next(new ErrorHandler("Please provide your preffered Job niches.", 400))
+        }
+    } catch (error) {
+        
+    }
+})
