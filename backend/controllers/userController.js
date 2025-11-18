6d8b9122375661d0f26a7b5236247c8bdbe1920e
Usfahussain493 <bc200203561@vu.edu.pk>
2025-11-18 12:59:36 +0500

    Updated userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import {catchAsyncErrors} from "../middlewares/catchAsyncError.js";
 import ErrorHandler from "../middlewares/error.js";
 import { User } from "../models/userSchema.js";
+import {v2 as cloudinary} from "cloudinary";
 
 export const register = catchAsyncErrors(async(req, res, next)=>{
     try {
@@ -23,6 +24,51 @@ export const register = catchAsyncErrors(async(req, res, next)=>{
         if(role === "Job Seeker" && (!firstNiche || !secondNiche || !thirdNiche)){
             return next(new ErrorHandler("Please provide your preffered Job niches.", 400))
         }
+
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return next(new ErrorHandler("Email is already registered.", 400))
+        }
+
+        const userData = {
+            name, 
+            email,
+            phone,
+            address,
+            password, 
+            role, 
+            niches:{
+            firstNiche, 
+            secondNiche, 
+            thirdNiche,
+            },
+            coverLetter,
+        };
+
+        if(req.files && req.files.resume){
+            const {resume}= req.files;
+            if(resume){
+                try {
+                    const cloudinaryResponse =await cloudinary.uploader.upload(resume.tempFilesPath,
+                        {folder: "Job_Seeker_Resume"}
+                    ) 
+                    if(!cloudinaryResponse || cloudinaryResponse.error){
+                        return next(
+                            new ErrorHandler("Failed toupload resume to cloud.", 500)
+                        );
+                    };
+
+                    userData.resume = {
+                        public_id: cloudinaryResponse.public_id,
+                        url:cloudinaryResponse.secure_url
+                    };
+                    
+                }catch (error) {
+                    return next(new ErrorHandler("Failed to upload resume.", 500));
+                }
+            }
+        }
+
     } catch (error) {
         
     }
