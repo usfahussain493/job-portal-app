1e55cc3c9bdc33ddd5156e4183290a3b5b858187
Usfahussain493 <bc200203561@vu.edu.pk>
2025-11-19 13:55:21 +0500

    Updated config, models and utils

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -52,4 +52,12 @@ const userSchema = new mongoose.Schema({
     },
 });
 
-export const User = mongoose.model("User", userSchema)
+
+
+userSchema.methods.getJwtToken = function(){
+    return  jwt.sign({id: this._id},process.env.JWT_SECRET_KEY, {
+        expiresIn: process.env.JWT_EXPIRE,
+    });
+};
+
+export const User = mongoose.model("User", userSchema);
