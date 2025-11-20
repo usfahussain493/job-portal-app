f868eccf328471f55f7da0a78a5c600f97a73da8
Usfahussain493 <bc200203561@vu.edu.pk>
2025-11-20 12:28:14 +0500

    Updated controllers,routes

--- backend/routes/jobRouter.js
+++ backend/routes/jobRouter.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { isAuthenticated, isAuthorized } from "../middlewares/auth";
-import { postJob } from "../controllers/jobController";
+import { isAuthenticated, isAuthorized } from "../middlewares/auth.js";
+import { deleteJob, getAllJobs, getASingleJob, getMyJobs, postJob } from "../controllers/jobController.js";
 
 const router = express.Router();
 
-router.post("/post", isAuthenticated, isAuthorized("Employer", postJob));
+router.post("/post", isAuthenticated,  isAuthorized("Employer"),  postJob);
+router.get("/getall", getAllJobs);
+router.get("/getmyjobs", isAuthenticated, isAuthorized("Employer"), getMyJobs);
+router.delete("/delete/:id", isAuthenticated, isAuthorized("Employer"), deleteJob);
+router.get("/get/:id", isAuthenticated, getASingleJob);
 
 export default router;
