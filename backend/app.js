f0284fcd33471c3a361e8218430a6538ce19ad75
Usfahussain493 <bc200203561@vu.edu.pk>
2025-11-28 12:06:54 +0500

    Updated tmp , config, automation, app and utils

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ import fileUpload from "express-fileupload";
 import userRouter  from "./routes/userRouter.js";
 import jobRouter from "./routes/jobRouter.js";
 import applicationRouter from "./routes/applicationRouter.js";
+import { newsLetterCron } from "./automation/newsLetterCron.js";
 
 const app = express(); 
 config({path:"./config/config.env"})
@@ -35,6 +36,7 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
 
+newsLetterCron();
 connection();
 
 app.use(errorMiddleware)
--- backend/automation/newsLetterCron.js
+++ backend/automation/newsLetterCron.js
@@ -1,8 +1,37 @@
 import cron from "node-cron";
+import { Job } from "../models/jobSchema.js";
+import { User } from "../models/userSchema.js";
+import { sendEmail } from "../utils/sendEmail.js";
 
 
 export const newsLetterCron = ()=>{
-    cron.schedule("*/1 * * */ *", async()=>{
-        console.log("Running News Letter Cron Automation.")
-    })
-}
+    cron.schedule("*/1 * * * *", async()=>{
+      console.log("Running Crone Automation.")
+        const jobs = await Job.find({newsLetterSent: false});
+        for(const job of jobs){
+          try {
+            const filteredUsers = await User.find({
+              $or:[
+                {"niches.firstNiche": job.jobNiche},
+                {"niches.secondNiche": job.jobNiche},
+                {"niches.thirdNiche":job.jobNiche},
+              ]
+            })
+            for(const user of filteredUsers){
+              const subject = `Hot Job Alert: ${job.title} in ${job.jobNiche} Available Now`;
+              const message = `Hi ${user.name}, \n\nGreat news! A new job that fits your niche has just been posted. The position is for a ${job.title} with ${job.companyName}, and they are looking to hire immediately. \n\nJob Details:\n- **Position:** ${job.title}\n- **Company:** ${job.companyName}\n- **Location:** ${job.location}\n- **Salary:** ${job.salary}\n\nDon't wait too long! Job openings like!\n\nBest Regards, \nNicheNest Team`;
+              await sendEmail({
+                email: user.email,
+                subject,
+                message
+              });
+            }
+            job.newsLetterSent = true,
+            await job.save();
+          } catch (error) {
+            console.log("ERROR IN NODE CRON CATCH BLOCK");
+console.error("CRON ERROR:", error);
+          }  
+        }
+    });
+};
